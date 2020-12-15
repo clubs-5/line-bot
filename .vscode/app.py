9b33d3b6e09b1512aa137bf97950ed9582de87c6
1minl <69282515+1minl@users.noreply.github.com>
2020-12-16 01:25:57 +0800

    5

--- .vscode/app.py
+++ .vscode/app.py
@@ -13,8 +13,9 @@ from linebot.models import (
 )
 
 app = Flask(__name__)
-
+# Channel Access Token
 line_bot_api = LineBotApi('KH0oUZctJ5HEkTaJ8LsDewWB3efij6CtAJa1Sr6Nrp+LtzREzjZS43aUZJxkWdxary0Yhkyyq0hkmmx/m/GeBmD4qJZqCZWQ9oNFyD/yQDoU2unJKaXNYdmUVYrgnFM0VPlct1UtmfRo9TjiaBxcKAdB04t89/1O/w1cDnyilFU=')
+# Channel Secret
 handler = WebhookHandler('e5d41aa80b555092dcc2b140fff4c99c')
 
 
@@ -22,18 +23,15 @@ handler = WebhookHandler('e5d41aa80b555092dcc2b140fff4c99c')
 def callback():
     # get X-Line-Signature header value
     signature = request.headers['X-Line-Signature']
-
     # get request body as text
     body = request.get_data(as_text=True)
     app.logger.info("Request body: " + body)
-
     # handle webhook body
     try:
         handler.handle(body, signature)
     except InvalidSignatureError:
         print("Invalid signature. Please check your channel access token/channel secret.")
         abort(400)
-
     return 'OK'
 
 #處理訊息
@@ -47,7 +45,7 @@ def handle_message(event):
         line_bot_api.reply_message(event.reply_token, message)
     else:
         message = TextMessage(text=msg)
-        line_bot_api.reply_message(reply_token,message)
+        line_bot_api.reply_message(event.reply_token,message)
 
 
 if __name__ == "__main__":
